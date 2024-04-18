6031bbb80cf7b3af4cb5279a9cc0c5027c354f2d
kevkama <nkamauk7@gmail.com>
2024-04-18 14:08:06 +0300

    final

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,11 +1,9 @@
 import 'package:app_bar_with_search_switch/app_bar_with_search_switch.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:identicare/components/my_drawer.dart';
 import 'package:identicare/components/post_wall.dart';
-import 'package:identicare/screens/postpage.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
